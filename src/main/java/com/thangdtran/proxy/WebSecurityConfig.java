c0ace051190361bec58715f89f76d7d8ce235c51
Thang Tran <thangdtran@kms-technology.com>
2020-04-08 15:33:23 +0700

    update security walk through proxy

--- src/main/java/com/thangdtran/proxy/WebSecurityConfig.java
+++ src/main/java/com/thangdtran/proxy/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/home")
+                .antMatchers("/", "/home", "/user/**", "/product/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
